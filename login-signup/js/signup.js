89633338a739ae7ba4c3b6998099edd81564cefe
thedevaman <devaman2508@gmail.com>
2025-12-19 09:40:01 +0530

    validation for signupp

--- login-signup/js/signup.js
+++ login-signup/js/signup.js
@@ -1,6 +1,21 @@
 const createuser = (e) => {
 e.preventDefault()
-alert()
+validation("fullname","fullname-error")
+validation("email","email-error")
+validation("password","password-error")
+const fullname = document.getElementById('fullname').value.trim()
+const email = document.getElementById('email').value.trim()
+const password = document.getElementById('password').value.trim()
+const payload =JSON.stringify({
+  fullname:fullname, // fullname
+  email:email,// email
+  password:password, //password
+
+})
+const key = Date.now()
+localStorage.setItem(key,payload)
+
+
 }
 
 const validation = (inputId, errorId) =>{
@@ -10,6 +25,10 @@ const value = input.value.trim()
 if(value.length == 0)
 {
   lable.classList.remove("hidden")
+  lable.innerHTML = `${input.name} *is required`
+  throw new Error(`${input.name} *is required`)
+}else{
+   lable.classList.add("hidden")
   lable.innerHTML = `${input.name} *is required`
 }
 
