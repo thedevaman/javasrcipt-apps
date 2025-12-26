cb1b26d2a4f0daf3884d9f94a89c95513132fa5d
thedevaman <devaman2508@gmail.com>
2025-12-26 09:40:26 +0530

    setTime(( )=>{ },time)

---
+++ timer/script/script.js
@@ -0,0 +1,10 @@
+
+// settimeout used for delay (its run after given time only once)
+setTimeout(()=>{
+
+     Swal.fire({
+        html:`<img src="images/ads.png" alt="ads" width="100%"/>`,
+        showConfirmButton: false
+    })
+
+},5000)
