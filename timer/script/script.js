5aa5b2b3aecb94384d5a72b04c403f66f16f139d
thedevaman <devaman2508@gmail.com>
2025-12-27 23:25:07 +0530

    setTimeout() && setInterval

--- timer/script/script.js
+++ timer/script/script.js
@@ -20,10 +20,50 @@
 
 const download = ()=>{
 
-    const a = document.createElement('a')
-    a.href = "images/ads.png"
-    a.download = "ads.png"
-    a.click()
-    a.remove()
+   
+     let s = 5
+    const timer = document.getElementById('timer')
+   const interval = setInterval(()=>{
+     s = s-1
+     timer.innerHTML = `your download will start in ${s} sec`
+     if(s == 0)
+     {
+        clearInterval(interval)
+         const a = document.createElement('a')
+         a.href = "images/ads.png"
+         a.download = "ads.png"
+         a.click()
+         a.remove()
+         timer.innerHTML = ''
+     }
+    },1000)
+    timer.innerHTML = "your download will start in 05 sec"
+
+}
+
+const login = (e) => {
+e.preventDefault()
+const username  = document.getElementById('email').value.trim()
+const password  = document.getElementById('password').value.trim()
+
+if(username == "test@gmail.com" && password == "123456")
+{
+ new Swal({
+        icon : 'success',
+        title: 'Login Success!',
+        text : "Please wait for redirection!"
+    })   
+
+    setTimeout(()=>{
+        location.href = 'https://www.google.com/'
+    },5000)
+
+}else{
+    new Swal({
+        icon : 'error',
+        title: 'Login Failed!'
+    })
+}
+
 
 }
