379117e8d38da1d030259ba95cadd61305ae3f25
thedevaman <devaman2508@gmail.com>
2026-01-01 00:34:47 +0530

    carosal

---
+++ carousel/js/script.js
@@ -0,0 +1,22 @@
+const slider = document.getElementById('slider')
+var size = 940
+var move = 0
+const totalSize = size * 4
+
+const next = () =>{
+    if(move < totalSize-size)
+    {
+        move = move+size
+        slider.style.marginLeft = `-${move}px`
+    }
+   
+}
+
+const prev = () =>{
+if(move >= size)
+{
+move = move-size
+slider.style.marginLeft = `${move}px`
+}
+   
+}
