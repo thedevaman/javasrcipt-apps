dc599c4c01f9a9b9efe1ea057cf88510d68fc0a5
thedevaman <devaman2508@gmail.com>
2026-01-04 23:21:32 +0530

    modal created

---
+++ modal/js/script.js
@@ -0,0 +1,8 @@
+const modal = document.getElementById('modal')
+const openModal = () =>{ 
+    modal.classList.remove('hidden')
+}
+
+const closeModal = () =>{ 
+    modal.classList.add('hidden')
+}
