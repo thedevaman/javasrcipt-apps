02a70d494f01b616170657726c44a4a2cd6847be
thedevaman <devaman2508@gmail.com>
2026-01-05 00:09:59 +0530

    accordian (collapse added)

--- modal/js/script.js
+++ modal/js/script.js
@@ -6,3 +6,16 @@ const openModal = () =>{
 const closeModal = () =>{ 
     modal.classList.add('hidden')
 }
+
+const toggleCollapse =()=>{
+    const collapse = document.getElementById('collapse')
+    const height = collapse.style.height
+    if(height == "0px")
+    {
+    collapse.style.height = '150px'
+    collapse.style.padding = '16px'
+    }else{
+        collapse.style.height = '0px'
+        collapse.style.padding = '0px'
+    }
+}
