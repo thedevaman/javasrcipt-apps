03e28e4d2e284754f8028ed849f3e4ed58d69507
thedevaman <devaman2508@gmail.com>
2026-01-11 00:05:42 +0530

    responsive side bar

---
+++ admin-panel/script/navbar.js
@@ -0,0 +1,14 @@
+
+const aside = document.getElementById('aside')
+const openMobileMenu = () =>{
+
+    
+        aside.style.right = 0
+
+}
+
+const closeMobileMenu = () =>{
+
+        aside.style.right = '-280px'
+
+}
